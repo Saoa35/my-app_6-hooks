2740f96904efa3eba199470055a3775b38c24ad7
Saoa35 <saoamah@gmail.com>
2022-10-01 23:25:33 +0200

    + fetch

--- src/components/EffectHookEx/EffectHookEx.js
+++ src/components/EffectHookEx/EffectHookEx.js
@@ -1,15 +1,28 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
 
 const EffectHookEx = () => {
 
-    useEffect()
+  const [data, setData] = useState();
 
-    return(
-        <div>
-            <h2>EffectHookEx</h2>
-        </div>
-    )
+  useEffect(() => {
+    console.log('render in useEffect');
+
+    const url = 'https://jsonplaceholder.typicode.com/todos/';
+
+      fetch(url)
+        .then(res => res.json())
+        .then(data => setData(data));
+
+  });
+
+  console.log('render');
+
+  return(
+    <div>
+      <h2>EffectHookEx</h2>
+    </div>
+  )
 }
 
 export default EffectHookEx;
