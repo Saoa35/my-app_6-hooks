15b2068340a26984a43a9bd14d102fbb1fcf877a
Saoa35 <saoamah@gmail.com>
2022-10-02 10:56:36 +0200

    + ItemsList comp

--- src/components/CallbackHookEx/CallbackHookEx.js
+++ src/components/CallbackHookEx/CallbackHookEx.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const ItemsList = ({getData}) => {
+
+    const [state, setState] = useState(null);
+
+    useEffect(() => {
+        const data = getData();
+        setState(data);
+    }, [getData])
+
+    return (
+        <ul>
+            {state?.map(el => <li key={el}>{el}</li>)} 
+        </ul>
+    )
+}
 
 const CallbackHookEx = () => {
 
+    const [state, setState] = useState(1);
+    const [state2, setState2] = useState(1);
+
+    const getData = () => {
+        console.log('render!');
+
+        return new Array(state).fill(null).map((_,idx) => `Element ${idx + 1}`);
+    }
+
     return(
         <div>
-            <h2>CallbackHookEx</h2>
+            <h3>CallbackHookEx</h3>
+            <ItemsList getData={getData}/>
         </div>
     )
 }
