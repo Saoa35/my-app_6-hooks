11ab0cca4c9a1382e4e89c8e21981a37147dfb19
Saoa35 <saoamah@gmail.com>
2022-10-02 10:29:14 +0200

    + useMemo

--- src/components/MemoHookEx/MemoHookEx.js
+++ src/components/MemoHookEx/MemoHookEx.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import  { useState } from 'react';
+import React, { useMemo, useState } from 'react';
+
 
 const computedFunc = (state) => {
   return state * 2;
@@ -11,17 +11,18 @@ const MemoHookEx = () => {
   const [state2, setState2] = useState(1);
 
 
-  const computed = computedFunc(state);
+  const computed = useMemo(() => computedFunc(state), [state]);
 
   return(
     <div>
       <h2>MemoHookEx</h2>
       <h4>
-        comp: {computed}
+        comp: {computed} comp2: {state2}
       </h4>
 
       <p>
         <button onClick={() => setState(computed + 1)}>Click!</button>
+        <button onClick={() => setState2(state2 + 1)}>Click 2!</button>
       </p>
     </div>
   )
