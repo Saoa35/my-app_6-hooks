2c97574d769c3d592181c0b321aa458dde075760
Saoa35 <saoamah@gmail.com>
2022-10-02 10:15:12 +0200

    + setState2

--- src/components/MemoHookEx/MemoHookEx.js
+++ src/components/MemoHookEx/MemoHookEx.js
@@ -8,12 +8,21 @@ const computedFunc = (state) => {
 const MemoHookEx = () => {
 
   const [state, setState] = useState(1);
+  const [state2, setState2] = useState(1);
+
 
   const computed = computedFunc(state);
 
   return(
     <div>
       <h2>MemoHookEx</h2>
+      <h4>
+        comp: {computed}
+      </h4>
+
+      <p>
+        <button onClick={() => setState(computed + 1)}>Click!</button>
+      </p>
     </div>
   )
 }
